5afa8612b47e51ca7d1331eb82a78998532facda
drone <drone@arparrot.(none)>
2013-06-12 12:18:37 -0800

    Small updates.

--- kinect/coordinate_system.hpp
+++ kinect/coordinate_system.hpp
@@ -7,16 +7,16 @@
 #define kcs_desired_height 0.0
 
 //offset from kinect to the center of box
-#define kcs_kinect_offset vec3(0.0,0.5,0.0)
+#define kcs_kinect_offset vec3(0.3,0.0,0.0)
 
 //Size of viewable field (full size of field, not 1/2 or 1/4 or distance+1/2 of...)
-#define kcs_x_field_size 3.1
-#define kcs_y_field_size 1.75
-#define kcs_z_field_size 3.5
+#define kcs_x_field_size 2.5//3.1
+#define kcs_y_field_size 1.75//1.75
+#define kcs_z_field_size 1//3.5
 #define kcs_buffer 0.3
 
 //Distance FROM kinect to edge of field
-#define kcs_distance_from_kinect 3.0
+#define kcs_distance_from_kinect 1.5//3.0
 
 //Distance TO origin (center of field)
 #define kcs_distance_to_origin (kcs_z_field_size/2.0+kcs_distance_from_kinect)
