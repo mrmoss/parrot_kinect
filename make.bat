19399b564692785b876addf732fd45ce49930a0f
Mike Moss <mrmoss@alaska.edu>
2013-06-13 15:10:36 -0800

    The make.bat for windows should work as soon as I get a freaking prebuilt freenect binary for Windows...  WHY HASN'T ANYONE UPLOADED A FREAKING BINARY?!  >_< T_T

--- make.bat
+++ make.bat
@@ -1,48 +1,45 @@
+@echo off
 REM COMPILER VARIABLES
 	SET OPTS=-O
 	SET COMPILER=g++
-	SET OUT=../../bin
-	SET SRC_PARROT=../../src/main.cpp ../../src/msl/*.cpp ../../src/falconer.cpp ../../src/raw_to_jpeg.cpp ../../src/kinect/libfree/*.cpp ../../src/kinect/Kinect.cpp ../../src/PDController.cpp ../../src/PIDController.cpp
-	SET CFLAGS_PARROT=-Wall -std=c++0x %OPTS%
-	SET LIB_PARROT=-lGL -lGLU -lGLEW -lglui -lglut -lSOIL -lavcodec -lswscale -ljpeg -lfreenect
+	SET SRC_SOIL=../include/SOIL/SOIL.c ../include/SOIL/stb_image_aug.c
+	SET SRC_MSL=%SRC_SOIL% ../../src/msl/2d.cpp ../../src/msl/2d_util.cpp ../../src/msl/file_util.cpp ../../src/msl/font.cpp ../../src/msl/glut_input.cpp ../../src/msl/socket.cpp ../../src/msl/socket_util.cpp ../../src/msl/sprite.cpp ../../src/msl/string_util.cpp ../../src/msl/time_util.cpp
+	SET SRC_PARROT=%SRC_MSL% ../../src/main.cpp ../../src/falconer.cpp ../../src/raw_to_jpeg.cpp ../../src/kinect/libfree/*.cpp ../../src/kinect/Kinect.cpp ../../src/PDController.cpp ../../src/PIDController.cpp
+	SET CFLAGS_PARROT=-Wall -Wno-unknown-pragmas -lpthread -std=c++11 -lm %OPTS%
+	SET LIB_PARROT=-lopengl32 -lglu32 -lfreeglut -lglew32 -lavcodec -lswscale -ljpeg -lwsock32
 	SET SRC_KINECT=../../src/kinect/main.cpp ../../src/kinect/Kinect.cpp ../../src/kinect/libfree/*.cpp
 	SET CFLAGS_KINECT=-Wall %OPTS%
-	SET LIB_KINECT=-lGL -lGLU -lGLEW -lglui -lglut -lSOIL -lfreenect
+	SET LIB_KINECT=-lopengl32 -lglu32 -lfreeglut -lglew32
 
 REM GO TO COMPILER DIRECTORY
-	cd g++/bin
-
-REM GET NUMBER OF COMMAND LINE ARGUMENTS
-	set argC=0
-	for %%x in (%*) do Set /A argC+=1
+	CD g++/bin
 
 REM BUILD VARIABLES
-	SET /a BUILD_PARROT=0
-	SET /a BUILD_PARROT=0
-	SET /a BUILD_CLEAN=0
-
-REM EMPTY MAKE ARGUMENTS MEANS MAKE ALL
-	IF %argC%==0 SET /a BUILD_PARROT=1
-	IF %argC%==0 SET /a BUILD_KINECT=1
+	SET BUILD_PARROT=0
+	SET BUILD_KINECT=0
+	SET BUILD_CLEAN=0
 
 REM CHECK COMMAND LINE OPTIONS
-	SET /a i=1
-	:LOOP
-		IF %i%==%argC% GOTO END
-		IF %%i%=="all" SET /a BUILD_PARROT=1
-		IF %%i%=="all" SET /a BUILD_KINECT=1
-		IF %%i%=="parrot" SET /a BUILD_PARROT=1
-		IF %%i%=="kinect" SET /a BUILD_KINECT=1
-		IF %%i%=="clean" SET /a BUILD_CLEAN=1
-		IF %%i%=="clean" GOTO END
-		SET /a i=%i%+1
-		GOTO LOOP
-	:END
+	SET ARGC=0
+	FOR %%i IN (%*) do (
+		SET /a ARGC+=1
+		IF %%i==all SET BUILD_PARROT=1
+		IF %%i==all SET BUILD_KINECT=1
+		IF %%i==parrot SET BUILD_PARROT=1
+		IF %%i==kinect SET BUILD_KINECT=1
+		IF %%i==clean SET BUILD_CLEAN=1
+	)
+
+	IF %ARGC%==0 SET BUILD_PARROT=1
+	IF %ARGC%==0 SET BUILD_KINECT=1
 
 REM BUILD WHAT IS NEEDED
-	IF %BUILD_PARROT%==1 %COMPILER% %CFLAGS_PARROT% %SRC_PARROT% -o %OUT%/parrot.exe %LIB_PARROT%
-	IF %BUILD_KINECT%==1 %COMPILER% %CFLAGS_KINECT% %SRC_KINECT% -o %OUT%/kinect.exe %LIB_KINECT%
-	IF %BUILD_CLEAN%==1	del %OUT%/parrot.exe %OUT%/kinect.exe
+	IF %BUILD_PARROT%==1 %COMPILER% %CFLAGS_PARROT% %SRC_PARROT% -o ../../bin/parrot.exe %LIB_PARROT%
+	IF %BUILD_KINECT%==1 %COMPILER% %CFLAGS_KINECT% %SRC_KINECT% -o ../../bin/kinect.exe %LIB_KINECT%
+	IF %BUILD_CLEAN%==1 CD ../../bin
+	IF %BUILD_CLEAN%==1 DEL /Q parrot.exe
+	IF %BUILD_CLEAN%==1 DEL /Q kinect.exe
+	IF %BUILD_CLEAN%==1 CD ../g++/bin
 
 REM GO BACK TO ORIGINAL DIRECTORY
-	cd ../../
+	CD ../../
